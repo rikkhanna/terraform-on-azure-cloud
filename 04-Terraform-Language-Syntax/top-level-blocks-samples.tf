c4145e59ead1ee025f671320ca4dff8765d13f8a
unknown <rishabhkhanna726@gmail.com>
2022-09-23 21:46:56 +0530

    lab-04

--- 04-Terraform-Language-Syntax/top-level-blocks-samples.tf
+++ 04-Terraform-Language-Syntax/top-level-blocks-samples.tf
@@ -9,12 +9,12 @@ terraform {
     }
   }
 # Terraform State Storage to Azure Storage Container
-  backend "azurerm" {
-    resource_group_name   = "terraform-storage-rg"
-    storage_account_name  = "terraformstate092022"
-    container_name        = "tfstatefiles"
-    key                   = "terraform.tfstate"
-  }   
+  # backend "azurerm" {
+  #   resource_group_name   = "terraform-storage-rg"
+  #   storage_account_name  = "terraformstate092022"
+  #   container_name        = "tfstatefiles"
+  #   key                   = "terraform.tfstate"
+  # }   
 }
 
 #####################################################################
